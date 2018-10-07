20d7e47e7531369c1e9767145accb4949940c72c
steven real sanchez <stevenn9549@gmail.com>
2018-10-07 05:01:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 // Add your functions and code here
 
-function destructivelyAppendKitten(name) {
+function destructivelyAppendKitten(kitten){
   return kittens.push(Ralph);
 }
