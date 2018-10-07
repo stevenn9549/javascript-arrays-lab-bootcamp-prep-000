b72a1ce391b4c5e6c63189a267a99dfd60957c1b
steven real sanchez <stevenn9549@gmail.com>
2018-10-07 05:43:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function prependKitten(Arnold){
   return ["Arnold", ...kittens];
 }
 function removeLastKitten(){
-  return kittens.slice(0, kittens.lenght -1)
+  return kittens.slice(0, kittens.length -1)
 }
 
 
